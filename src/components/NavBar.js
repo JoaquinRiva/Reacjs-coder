0712f27128c2b21905391aa148fafa77a87c170d
Joaquin <rivajoaquin012@gmail.com>
2022-08-30 20:05:58 -0300

    Desafio-2

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,9 +1,31 @@
 
-const App = () => {
+const NavBar = () => {
     return(
-      <div>
-       NavBar
-      </div>
+       <nav class="navbar navbar-expand-lg bg-light">
+  <div class="container-fluid">
+    <a class="navbar-brand text-danger" href="#">HyperX</a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarNav">
+      <ul class="navbar-nav">
+        <li class="nav-item">
+          <a class="nav-link active" aria-current="page" href="#">Inicio</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">Destacados</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">Productos</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link">Contacto</a>
+        </li>
+      </ul>
+    </div>
+  </div>
+</nav>
+
     )
      }
 
